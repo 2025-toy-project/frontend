ebb748fcc77ae580fe984021f971b59e0d8b038c
Jiwon Park <parkjiwon0821@naver.com>
2025-01-10 22:05:30 +0900

    design: register step4 UI 구성

---
+++ src/components/register/RegisterStep4.jsx
@@ -0,0 +1,20 @@
+import 'styles/register/RegisterStep4.scss';
+import { ReactComponent as CircleIcon } from 'assets/icons/circle_icon.svg';
+
+
+const RegisterStep4 = () => {
+  return (
+    <div className="register-step4-container">
+      <div className="icon-container">
+        <CircleIcon className="circle-icon" />
+      </div>
+      <h1 className="title">가입 신청이 완료되었습니다!</h1>
+      <p className="description">
+        관리자 승인 후, 아이디는 개별 문자로 안내되며<br />
+        관리자 승인은 최대 n일까지 소요 될 수 있습니다.
+      </p>
+    </div>
+  );
+};
+
+export default RegisterStep4;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,6 +4,7 @@ import logo_url from '../assets/logo/logo_blue_miri.png';
 import RegisterStep1 from 'components/register/RegisterStep1';
 import RegisterStep2 from 'components/register/RegisterStep2';
 import RegisterStep3 from 'components/register/RegisterStep3';
+import RegisterStep4 from 'components/register/RegisterStep4';
 
 /**
  * RegisterPage 컴포넌트
@@ -24,7 +25,7 @@ const RegisterPage = () => {
             case 3:
                 return <RegisterStep3 />;
             case 4:
-                return <RegisterStep1 />;
+                return <RegisterStep4 />;
             default:
                 return null;
         }
@@ -52,7 +53,7 @@ const RegisterPage = () => {
                 <span className={`register-step ${currentStep === 1 ? 'active' : ''}`}>매장정보입력</span> &gt;
                 <span className={`register-step ${currentStep === 2 || currentStep === 3 ? 'active' : ''}`}>사장님정보입력</span> &gt;
                 <span className={`register-step ${currentStep === 4 ? 'active' : ''}`}>승인대기</span> &gt;
-                <span className={`register-step ${currentStep === 5 ? 'active' : ''}`}>가입완료</span>
+                <span className={`register-step ${currentStep >= 5 ? 'active' : ''}`}>가입완료</span>
             </nav>
 
             <div className="register-content">{renderStepComponent()}</div>
