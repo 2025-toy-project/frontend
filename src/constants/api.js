f002e4bbc7e043829076037c2a4c1bb622b9f7a6
whitecity01 <kh081104@gmail.com>
2025-01-16 07:07:17 +0900

    feat: api 키 파일 추가

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+// 개발용
+export const SERVER_IP = 'http://localhost:9091';
+
+export const ORDER_BASE_URL = 'owner/order';
+export const STORE_BASE_URL = 'owner/store';
+
+// 개발용
+export const JWT_AT = '';
